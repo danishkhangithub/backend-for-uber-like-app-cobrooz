c3087df0f2854498c9f8d3b0252d67d912ed361c
Danish Khan <41539301+danishkhangithub@users.noreply.github.com>
2021-02-28 10:02:58 +0500

    Add files via upload

---
+++ app/urls.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.urls import path, re_path
+from app import views
+
+urlpatterns = [
+
+    # The home page
+    path('', views.index, name='home'),
+    path('create/', views.index, name = 'create'),
+    
+
+    # Matches any html file
+    re_path(r'^.*\.*', views.pages, name='pages'),
+
+]
